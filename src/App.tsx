69c33a24cd3ef18d6cad4fa681b98e758e19079e
MelissaAyllon <mayllon@espol.edu.ec>
2024-06-19 10:59:58 -0500

    update: remove for ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Grid container spacing={5}>
       <Grid xs={12} sm={4} md={3} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
