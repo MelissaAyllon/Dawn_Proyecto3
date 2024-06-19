e3fc6118ae95e513bd96df5e52c0bca3d912530c
MelissaAyllon <mayllon@espol.edu.ec>
2024-06-19 09:43:07 -0500

    Update: MUI & components intro

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Indicator from './components/Indicator';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Grid>
-      <Grid>1</Grid>
-      <Grid>2</Grid>
-      <Grid>3</Grid>
-      <Grid>4</Grid>
-      <Grid>5</Grid>
-      <Grid>6</Grid>
+    <Grid container spacing={5}>
+      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>5</Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>6</Grid>
+      <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
 	  </Grid>
   )
 }
