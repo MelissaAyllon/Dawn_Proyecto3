a1249aeca2e0345333fdc64cda4bbe0e01d21f09
MelissaAyllon <mayllon@espol.edu.ec>
2024-06-24 10:58:15 -0500

    Guia 13

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
+import WeatherChart from './components/WeatherChart'; 
+import ControlPanel from './components/ControlPanel';
 import './App.css'
 
 function App() {
@@ -18,12 +20,20 @@ function App() {
           <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
         </Grid>
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>5</Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>
+      </Grid>
       <Grid xs={6} sm={4} md={3} lg={2}>6</Grid>
       <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
       <Summary></Summary>
       <BasicTable></BasicTable>
+      <Grid xs={12} lg={2}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} lg={10}>
+        <WeatherChart></WeatherChart>
+      </Grid>
 	  </Grid>
+    
   )
 }
 
