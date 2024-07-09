7a71fb14f582b4bdf457a08084ae004fab959bc8
MelissaAyllon <mayllon@espol.edu.ec>
2024-07-08 23:25:02 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Typography from '@mui/material/Typography';
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
@@ -100,10 +101,26 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-      <Grid xs={12} sm={4} md={3} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-      <Grid xs={6} sm={4} md={3} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-      <Grid xs={6} sm={4} md={3} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-      <Grid xs={12} sm={4} md={3} lg={4}> {/* De este grid, coloco los demás dentro porque */ }
+      <Grid xs={12} sm={12} lg={12} xl={12}>
+          <Typography component="h6" variant="h3" color="white">
+            Weather App
+          </Typography>
+      </Grid>
+
+      <Grid xs={12} sm={4} md={3} lg={4}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+
+      <Grid xs={6} sm={4} md={3} lg={4}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+
+      <Grid xs={6} sm={4} md={3} lg={4}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+
+      <Grid xs={12} sm={4} md={3} lg={4}> 
+        {/* De este grid, coloco los demás dentro porque */ }
         <Grid xs={6} sm={4} md={3} lg={12} sx={{paddingBottom:"5"}}>
           <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
         </Grid>
@@ -111,30 +128,46 @@ function App() {
           <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
         </Grid>
       </Grid>
+
       <Grid xs={6} sm={4} md={3} lg={2}>
-      {indicators[0]}
+        {indicators[0]}
       </Grid>
+      
       <Grid xs={6} sm={4} md={3} lg={2}>
-      {indicators[1]}
+        {indicators[1]}
       </Grid>
+
       <Grid xs={6} sm={4} md={3} lg={2}>
-      {indicators[2]}
+        {indicators[2]}
       </Grid>
+
       <Grid xs={6} sm={4} md={3} lg={2}>
-      {indicators[3]}
+        {indicators[3]}
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>6</Grid>
+
+      <Grid xs={6} sm={4} md={3} lg={2}>
+        6
+      </Grid>
+
       <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-      <Summary></Summary>
-      <BasicTable rows={rowsTable}></BasicTable>
+
+      <Summary>
+
+      </Summary>
+
+      <BasicTable rows={rowsTable}>
+        
+      </BasicTable>
+
       <Grid xs={12} lg={2}>
         <ControlPanel />
       </Grid>
+
       <Grid xs={12} lg={10}>
         <WeatherChart></WeatherChart>
       </Grid>
+
 	  </Grid>
-    
   )
 }
 
